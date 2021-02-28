5eb44e23b87936c8ba207d6860cd713b07bd488d
Henry Bonath <henry@thinkcsc.com>
2021-02-28 12:02:32 -0500

    Update for Python3 compatibility

--- inventory.py
+++ inventory.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Ansible dynamic inventory script for saltstack source
 """
@@ -16,7 +16,7 @@ try:
     import salt
     import salt.client
 except ImportError:
-    print "failed=True msg='`saltstack` is required for this script'"
+    print("failed=True msg='`saltstack` is required for this script'")
     sys.exit(1)
 
 def main():
@@ -27,11 +27,11 @@ def main():
     local = salt.client.LocalClient()
 
     if len(sys.argv) == 2 and sys.argv[1] == '--list':
-        print json.dumps(local.cmd('*', 'grains.items'), indent=4, sort_keys=True)
+        print(json.dumps(local.cmd('*', 'grains.items'), indent=4, sort_keys=True))
     elif len(sys.argv) == 3 and sys.argv[1] == '--host':
-        print json.dumps(local.cmd(sys.argv[2], 'grains.items'), indent=4, sort_keys=True)
+        print(json.dumps(local.cmd(sys.argv[2], 'grains.items'), indent=4, sort_keys=True))
     else:
-        print "Need an argument, either --list or --host <host>"
+        print("Need an argument, either --list or --host <host>")
 
 if __name__ == "__main__":
     main()
